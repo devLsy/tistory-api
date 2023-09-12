e6b530be56e3bb21f7137f51640c6f04544c3351
devlsy86 <infightersy@naver.com>
2023-09-12 21:20:46 +0900

    first commit

---
+++ src/main/java/com/lsy/tistoryapi/TistoryApiApplication.java
@@ -0,0 +1,13 @@
+package com.lsy.tistoryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TistoryApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TistoryApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lsy/tistoryapi/TistoryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsy.tistoryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TistoryApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
