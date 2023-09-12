1de4fa9e5fb281b197fa1a35a7f9aa324e747e8b
devlsy86 <infightersy@naver.com>
2023-09-12 21:42:57 +0900

    OpenAPI 정보 get 테스트

--- src/main/java/com/lsy/tistoryapi/board/web/BoardController.java
+++ src/main/java/com/lsy/tistoryapi/board/web/BoardController.java
@@ -1,17 +1,31 @@
 package com.lsy.tistoryapi.board.web;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @Slf4j
+@RequiredArgsConstructor
 @RequestMapping("")
 public class BoardController {
-    
+
+    @Value("${open-api.app-id}")
+    private String appId;
+
+    @Value("${open-api.secret-key}")
+    private String secretKey;
+
+    @Value("${open-api.access-token}")
+    private String accessToken;
+
+
     @GetMapping("/")
     public String home() {
+        log.info("accessToken = {}", accessToken);
         return "pages/index";
     }
 }
