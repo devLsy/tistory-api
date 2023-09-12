183bd0f94dc0252511cf0847b0623d4938517a67
devlsy86 <infightersy@naver.com>
2023-09-12 21:31:20 +0900

    화면 테스트

---
+++ src/main/java/com/lsy/tistoryapi/board/web/BoardRestController.java
@@ -0,0 +1,11 @@
+package com.lsy.tistoryapi.board.web;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequestMapping("")
+public class BoardRestController {
+}
